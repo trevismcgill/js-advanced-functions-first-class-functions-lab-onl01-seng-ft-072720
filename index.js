7be7803283720e2a3fc7e7bb0b5e5ae575076a7f
trevismcgill <trevismcgill25@gmail.com>
2020-11-09 16:13:00 -0600

    commit tree 1efae748331bd9a06250f5b7570a9e7033a96b4d

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array) {
+        let newArray = [] 
+        newArray.push(array[0])
+        newArray.push(array[1])
+        return newArray
+};
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+  
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+      return multiplyValue * value;
+    };
+  };
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, whichDrivers) {
+  return whichDrivers(drivers);
+};
+
+
